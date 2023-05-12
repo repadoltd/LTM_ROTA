71ba04fed67dd748677ca483aa8d3cd07f6f7b02
repadoltd <133341003+repadoltd@users.noreply.github.com>
2023-05-12 13:13:30 +0300

    Add files via upload

---
+++ REP_SMS/REP_SMS_CONTROLLER_F/REP_SMS_CONTROLLER_F/Class1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+
+namespace REP_SMS_CONTROLLER_F
+{
+    public class CL_SMS
+    {
+        public string _message;
+        public static CL_MSG _obj_cl_msg;
+
+
+        protected static string _user;
+        protected static string _password;
+        protected static string _python_exe_path;
+        protected static string _python_project_path_send;
+        protected static string _python_project_path_get;
+
+        public System.Timers.Timer _getSMSTimer = new System.Timers.Timer();
+
+
+
+
+        public CL_SMS(CL_MSG obj_cl_msg, string user, string password, string python_exe_path, string python_project_path_send, string python_project_path_get)
+        {
+            _user = user;
+            _password = password;
+            _obj_cl_msg = obj_cl_msg;
+            _python_exe_path = python_exe_path;
+            _python_project_path_send = python_project_path_send;
+            _python_project_path_get = python_project_path_get;
+
+
+
+            SetupTimer();
+        }
+
+        public void SendSMS(string phonenumber, string messagetext)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = _python_exe_path;
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+
+            string function = "SendSMS";
+            string argument = string.Format(_python_project_path_send, _user, _password, function, phonenumber, messagetext); // @"path\to\application.py {0} {1} {2} {3} {4}"
+            start.Arguments = argument;
+
+            Process process = Process.Start(start);
+            StreamReader reader = process.StandardOutput;
+            string result = reader.ReadToEnd();
+
+            if (result != "")
+            {}
+
+        }
+        public void GetSMS()
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = _python_exe_path;
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+
+            string function = "GetSMS";
+            string argument = string.Format(_python_project_path_get, _user, _password, function); // @"path\to\application.py {0} {1} {2}"
+            start.Arguments = argument;
+
+            Process process = Process.Start(start);
+            StreamReader reader = process.StandardOutput;
+            
+            string result = reader.ReadToEnd();
+
+            if (result != "")
+            {
+
+                _obj_cl_msg.PopulateDB(result);
+
+            }
+
+
+            
+
+
+        }
+        private void SetupTimer()
+        {
+            _getSMSTimer = new System.Timers.Timer();
+            _getSMSTimer.Interval = 1000;
+            _getSMSTimer.Elapsed += new System.Timers.ElapsedEventHandler(getSMSTimerElapsed);
+            _getSMSTimer.Enabled = true;
+
+            _getSMSTimer.Start();
+        }
+
+        void getSMSTimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _getSMSTimer.Stop();
+            try
+            {
+                GetSMS();
+            }
+            catch { }
+            _getSMSTimer.Start();
+        }
+
+
+
+
+    }
+
+    public class CL_MSG
+    {
+        public CL_EVENT _obj_event;
+
+
+
+
+        DataTable tab_message = new DataTable();
+        public CL_MSG()
+        {
+            CreateDB();
+            _obj_event = new CL_EVENT();
+        }
+
+        private void CreateDB()
+        {
+            tab_message.Columns.Add("Time", typeof(string));
+            tab_message.Columns.Add("Content", typeof(string));
+        }
+
+        public string GetTime(string messages)
+        {
+            string time = "";
+            Console.WriteLine(messages);
+            int from = messages.IndexOf(", 'Date': '");
+            int to = messages.LastIndexOf("', 'Sca':");
+
+            if(!(from <= 0 && to <= 0)) 
+            { 
+                time = messages.Substring(from, to - from).Replace(", 'Date': '", "").Trim(); 
+            }
+
+            
+            return time;
+        }
+
+        public string GetContent(string message)
+        {
+            string content = "";
+            int from = message.IndexOf(", 'Content': '");
+            int to = message.LastIndexOf("', 'Date': '");
+
+            if (!(from <= 0 && to <= 0))
+            { content = message.Substring(from, to - from).Replace(", 'Content': '", "").Trim(); }
+            return content;
+        }
+
+        public void PopulateDB(string message)
+        {
+            string time = GetTime(message);
+            string content = GetContent(message);
+
+            DataRow[] row = tab_message.Select(String.Format("Time = '{0}'", time));
+
+
+            tab_message.Rows.Add(time, content);
+            ParseMessage(content);
+
+
+        }
+
+        public void ParseMessage(string content)
+        {
+            string events = content.Substring(0, 2);
+
+            string parameter1 = content.Substring(2, 4);
+
+            string parameter2 = content.Substring(6);
+
+            switch (events)
+            {
+                case "00":
+                    _obj_event.EventTaskStarted(parameter1, parameter2);
+                    break;
+                case "01":
+                    _obj_event.EventTaskCompleted(parameter1, parameter2);
+                    break;
+                case "02":
+                    _obj_event.EventRemainingTaskTime(parameter1, parameter2);
+                    break;
+                case "03":
+                    _obj_event.EventRemainingSubstepTime(parameter1, parameter2);
+                    break;
+                case "04":
+                    _obj_event.EventSetCurrentStep(parameter1, parameter2);
+                    break;
+                case "05":
+                    _obj_event.EventDisplayIssue(parameter1, parameter2);
+                    break;
+                case "06":
+                    _obj_event.EventDismissIssue(parameter1, parameter2);
+                    break;
+                default: throw new Exception("False Format in Messagetext");
+            }
+        }
+    }
+
+    public class CL_EVENT
+    {
+        public event TaskStartedEventHandler TaskStarted;
+        public delegate void TaskStartedEventHandler(string parameter1, string parameter2);
+
+        public event TaskCompletedEventHandler TaskCompleted;
+        public delegate void TaskCompletedEventHandler(string parameter1, string parameter2);
+
+        public event RemainingTaskTimeEventHandler RemainingTaskTime;
+        public delegate void RemainingTaskTimeEventHandler(string parameter1, string parameter2);
+
+        public event RemainingSubstepTimeEventHandler RemainingSubstepTime;
+        public delegate void RemainingSubstepTimeEventHandler(string parameter1, string parameter2);
+
+        public event SetCurrentStepEventHandler SetCurrentStep;
+        public delegate void SetCurrentStepEventHandler(string parameter1, string parameter2);
+
+        public event DisplayIssueReceivedEventHandler DisplayIssueReceived;
+        public delegate void DisplayIssueReceivedEventHandler(string parameter1, string parameter2);
+
+        public event DismissIssueEventHandler DismissIssue;
+        public delegate void DismissIssueEventHandler(string parameter1, string parameter2);
+
+
+
+
+
+
+
+
+
+
+        public void EventTaskStarted(string parameter1, string parameter2)
+        {
+
+            if (TaskStarted != null)
+            {
+                TaskStarted.Invoke(parameter1, parameter2);
+            }
+
+        }
+
+        public void EventTaskCompleted(string parameter1, string parameter2)
+        {
+
+            if (TaskCompleted != null)
+            {
+                TaskCompleted.Invoke(parameter1, parameter2);
+            }
+
+        }
+
+        public void EventRemainingTaskTime(string parameter1, string parameter2)
+        {
+
+            if (RemainingTaskTime != null)
+            {
+                RemainingTaskTime.Invoke(parameter1, parameter2);
+            }
+
+        }
+
+        public void EventRemainingSubstepTime(string parameter1, string parameter2)
+        {
+
+            if (RemainingSubstepTime != null)
+            {
+                RemainingSubstepTime.Invoke(parameter1, parameter2);
+            }
+
+        }
+
+        public void EventSetCurrentStep(string parameter1, string parameter2)
+        {
+
+            if (SetCurrentStep != null)
+            {
+                SetCurrentStep.Invoke(parameter1, parameter2);
+            }
+
+        }
+
+        public void EventDisplayIssue(string parameter1, string parameter2)
+        {
+
+            if (DisplayIssueReceived != null)
+            {
+                DisplayIssueReceived.Invoke(parameter1, parameter2);
+            }
+
+        }
+
+        public void EventDismissIssue(string parameter1, string parameter2)
+        {
+
+            if (DismissIssue != null)
+            {
+                DismissIssue.Invoke(parameter1, parameter2);
+            }
+
+        }
+    }
+}
---
+++ REP_SMS/REP_SMS_PROGRAM/REP_GET_SMS_PROG/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace REP_GET_SMS_PROG
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(0, 0);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(100, 27);
+            this.text1.TabIndex = 0;
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(0, 0);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(100, 27);
+            this.text2.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(1099, 355);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 368);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(1099, 34);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 502);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
